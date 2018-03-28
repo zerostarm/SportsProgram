1eec6e5be9d1c1d91cda8419586ba1a83f14c88f
zerostarm <zerostarm@users.noreply.github.com>
2018-03-28 13:16:08 -0400

    Easter Egg

---
+++ EasterEgg.java
@@ -0,0 +1,25 @@
+
+/**
+ * TODO Put here a description of what this class does.
+ *
+ * @author Zeros.
+ *         Created Mar 28, 2018.
+ */
+public class EasterEgg {
+    EasterEgg(){
+        System.out.println(""+
+"                 .-^-.
+"               .'=^=^='.
+"              /=^=^=^=^=\
+"      .-~-.  :^= HAPPY =^;
+"    .'~~*~~'.|^ EASTER! ^|
+    /~~*~~~*~~\^=^=^=^=^=^:
+   :~*~~~*~~~*~;\.-*))`*-,/
+   |~~~*~~~*~~|/ * ((*   *'.
+   :~*~~~*~~~*|   *))  *   *\
+    \~~*~~~*~~| *  ((*   *  /
+     `.~~*~~.' \  *))  *  .'
+jgs    `~~~`    '-.((*_.-'
+                  );
+    }
+}
