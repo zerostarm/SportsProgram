234cf8071530f6fa42d19f4ba5040b3b387a8378
zerostarm <zerostarm@users.noreply.github.com>
2018-03-28 12:58:17 -0400

    Stephen
    Finished Basketball

--- Basketball.java
+++ Basketball.java
@@ -6,29 +6,32 @@
  * @version 3-27-18
  */
 public class Basketball extends Sport {
-    private int two, twoTotal, three, threeTotal, assists;
-    
-    public void setAttrOne() {
-        System.out.println("Enter the total number of two-point attempts this season:");
-        twoTotal = scan.nextInt();
-        System.out.println("Enter the number of two-point goals scored this season:");
-        two = scan.nextInt();
-    }
-    
-    public void setAttrTwo() {
-        System.out.println("Enter the total number of three-point attempts this season:");
-        threeTotal = scan.nextInt();
-        System.out.println("Enter the number of three-point goals scored this season:");
-        three = scan.nextInt();
-    }
-    
-    public void setAttrThree() {
-        System.out.println("Enter the total number of assists this season:");
-        assists = scan.nextInt();
-    }
-    
-    public String toString() {
-        String ret = printCommon();
-        return "";
-    }
+	private int two, twoTotal, three, threeTotal, assists;
+
+	public void setAttrOne() {
+		System.out.println("Enter the total number of two-point attempts this season:");
+		twoTotal = scan.nextInt();
+		System.out.println("Enter the number of two-point goals scored this season:");
+		two = scan.nextInt();
+	}
+
+	public void setAttrTwo() {
+		System.out.println("Enter the total number of three-point attempts this season:");
+		threeTotal = scan.nextInt();
+		System.out.println("Enter the number of three-point goals scored this season:");
+		three = scan.nextInt();
+	}
+
+	public void setAttrThree() {
+		System.out.println("Enter the total number of assists this season:");
+		assists = scan.nextInt();
+	}
+
+	public String toString() {
+		String ret = printCommon();
+		ret += "\nTwo Point percentage: " + ((float) two / twoTotal*100) + "%";
+		ret += "\nThree Point percentage: " + ((float) three / threeTotal*100 + "%");
+		ret += "\nAssists per Game: " + ((float) assists / games);
+		return ret;
+	}
 }
